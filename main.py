34b7070bc2a35c7a6843d4acea6427922e8217d6
Muhammad Zaidi <qasimzaidi16@gmail.com>
2024-01-15 20:34:07 -0600

    finished :)

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-num1 = 4.6489
-num2 = 5.3283
-total = num1 + num2
-print(total)
+import test
 
+cube_1 = test.Cube
+
+cube_1.fresh_cube(cube_1)
+cube_1.scramble(cube_1)
+cube_1.display(cube_1)
+cube_1.white_cross(cube_1)
+cube_1.display(cube_1)
+cube_1.white_corners(cube_1)
+cube_1.display(cube_1)
+cube_1.second_layer(cube_1)
+cube_1.display(cube_1)
+cube_1.yellow_cross(cube_1)
+cube_1.display(cube_1)
+cube_1.yellow_edges(cube_1)
+cube_1.display(cube_1)
+cube_1.yellow_corners(cube_1)
+cube_1.display(cube_1)
+cube_1.final_step(cube_1)
+cube_1.display(cube_1)
+cube_1.show_solution(cube_1)
